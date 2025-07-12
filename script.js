721d1fd97d7f7fa4e13a61175f9b4cf044d2a8cd
Cristobal Ruiz <contacto@crinlorite.com>
2025-07-12 19:35:53 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,9 @@
 const canvas = document.getElementById("scratchCanvas");
 const ctx = canvas.getContext("2d");
 
-const message = "¿Quieres ser el padrino de Julieta?";
-ctx.font = "20px Arial";
-ctx.fillStyle = "#000";
-ctx.textAlign = "center";
-ctx.textBaseline = "middle";
-ctx.fillText(message, canvas.width / 2, canvas.height / 2);
-
-// Rellenamos con "rasca"
-ctx.globalCompositeOperation = "source-over";
-ctx.fillStyle = "#c0c0c0";
+ctx.fillStyle = "#c0c0c0"; // Color del rasca
 ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-// Activamos el borrado
 ctx.globalCompositeOperation = "destination-out";
 
 let isDrawing = false;
@@ -31,7 +21,7 @@ function draw(e) {
   e.preventDefault();
   const pos = getPosition(e);
   ctx.beginPath();
-  ctx.arc(pos.x, pos.y, 15, 0, Math.PI * 2);
+  ctx.arc(pos.x, pos.y, 20, 0, Math.PI * 2);
   ctx.fill();
 }
 
